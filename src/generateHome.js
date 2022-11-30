0fb5cf19ad00062dd247e4663ff1297d71203de5
Thamico <thanh.nguyenctn1999@gmail.com>
2022-11-30 18:11:58 +0100

    outsource html into js modules

---
+++ src/generateHome.js
@@ -0,0 +1,10 @@
+function generateHome() {
+  const content = document.querySelector("#content");
+  const div = document.createElement("div");
+
+  const subDiv = document.createElement("div");
+  const fieldHeader = document.createElement("fieldset");
+  const restaurantIMG = document.createElement("img");
+  restaurantIMG.setAttribute("src", "dist/images/5star.jpg");
+  restaurantIMG;
+}
