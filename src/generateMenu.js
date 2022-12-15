d0f2fdfc24086b84c1806c807e4aaf533b75b905
Thamico <thanh.nguyenctn1999@gmail.com>
2022-12-15 19:16:48 +0100

    implemented menu

--- src/generateMenu.js
+++ src/generateMenu.js
@@ -1,22 +1,12 @@
 function generateMenu() {
   const content = document.querySelector("#content");
-  const section = document.createElement("div");
 
-  section.classList.add("menu");
+  const menu = document.createElement("img");
+  menu.classList.add("imageM");
+  menu.setAttribute("src", "../dist/images/menu.jpg");
+  menu.setAttribute("alt", "menu");
 
-  const menuHeader = document.createElement("div");
-  menuHeader.classList.add("mintro");
-  const h1 = document.createElement("h1");
-  h1.textContent = "Menu";
-  menuHeader.appendChild(h1);
-
-  section.appendChild(menuHeader);
-  const menuDescription = document.createElement("div");
-  menuDescription.textContent =
-    "Unique wood fired pizzas or be tempted by our pasta, antipasto, focaccias, salads and Bruschetta dishes. Relax in the friendly ambience as you enjoy the aromas of the wood ovens or sit al fresco and soak up the Mildura sunshine.";
-
-  section.appendChild(menuDescription);
-  content.appendChild(section);
+  content.appendChild(menu);
 }
 
 export default generateMenu;
